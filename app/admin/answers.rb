940c007d007d38006f69d005fb053cc1bb239db0
Jon Lemmon <jon@loomio.org>
2013-09-08 17:58:15 +1200

    Redirect nicely after creating an answer

--- app/admin/answers.rb
+++ app/admin/answers.rb
@@ -4,6 +4,17 @@ ActiveAdmin.register Answer do
     @answer = Answer.new(question_id: params[:question_id])
   end
 
+  member_action :create do 
+    @answer = Answer.new(params[:answer])
+    if @answer.save
+      flash[:notice] = "Answer created for #{@answer.candidate.name}."
+      redirect_to new_admin_answer_path(question_id: @answer.question_id)
+    else
+      flash[:error] = "There were errors in the form. See below."
+      render action: :new
+    end
+  end
+
   form do |f|
     f.inputs "Content" do
       f.input :candidate
