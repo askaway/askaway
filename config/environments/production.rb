b4e7c8735f2ea227925258984b8501ff84495758
Jon Lemmon <jon@loomio.org>
2013-09-15 15:02:13 +1200

    Add mailer host info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Askaway::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => 'www.askaway.co.nz' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
