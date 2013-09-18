4c8f43e21f48029a811b47663a9c4e76e7e070c2
Jon Lemmon <jon@loomio.org>
2013-09-18 20:55:02 +1200

    Try fixing facebook like button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  def facebook_button(question)
-    content_tag(:div, "", class: "fb-like", data: {'like-url' => like_question_path(question), href: facebook_link(question), width: 100, layout: :button_count, 'show-faces' => false, send: true})
+  def facebook_button(question, send=false)
+    content_tag(:div, "", class: "fb-like", data: {'like-url' => like_question_path(question), href: facebook_link(question), width: 100, layout: :button_count, 'show-faces' => false, send: send})
   end
 
   def facebook_link(obj)
     host = (Rails.env.development?) ? 'example.com' : ENV['DEFAULT_URL_HOST']
-    url_for polymorphic_path(obj, host: host, port: nil, format: :html, only_path: false)
+    url_for polymorphic_path(obj, host: host, only_path: false)
   end
 
   def image_url(source)
