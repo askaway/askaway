23394645732ea95b3a1f655572dbf7a431a47283
Eduardo Gurgel <eduardo@gurgel.me>
2014-04-23 21:52:02 +1200

    Add FactoryGirl.lint to run before the test suite
    FactoryGirl.lint will check if every factory is valid before running the
    test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,12 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
   end
 
   config.before(:each) do
