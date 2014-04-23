ac0ee24e4897604e04b8e45124eeb632baa52df7
Rowan Crawford <rowan.crawford@joukou.co>
2014-04-23 19:25:25 +1200

    Run `bundle exec annotate"

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: candidates
+#
+#  id            :integer          not null, primary key
+#  name          :string(255)
+#  email         :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  authorisation :string(255)
+#  avatar        :string(255)
+#
+
 class Candidate < ActiveRecord::Base
   attr_accessible :name, :email, :avatar, :authorisation
 
