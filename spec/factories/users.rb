a0f534ee73b476a3d5b18437dab7a1fd05cfe5be
Artur Khantimirov <artur@carnivallabs.com>
2014-04-25 19:42:22 +1200

    feature spec for sing in and sign up

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
-    password { Faker::Internet.password(8) }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
