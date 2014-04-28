1c3ba2d2790a377d1baea634b15e7dcc2e8363a6
Caleb Tutty <caleb@prettymint.co.nz>
2014-04-28 22:54:22 +1200

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -99,7 +99,6 @@ ActiveRecord::Schema.define(:version => 20140428092306) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
