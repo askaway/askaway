94cdaa49279e96c1e019239b4db5912082cc56ca
Rowan Crawford <rowan.crawford@joukou.co>
2014-05-31 08:58:24 +1200

    Output as sql because there's db specific work

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Askaway
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
+    config.active_record.schema_format = :sql
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
