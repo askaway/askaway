a40bbea9f75d1c132802452795da5ea392954856
Rowan Crawford <rowan.crawford@joukou.co>
2014-05-31 08:31:24 +1200

    Create two different execute fns for debugging

--- db/migrate/20140529104412_create_popularity_function.rb
+++ db/migrate/20140529104412_create_popularity_function.rb
@@ -5,7 +5,9 @@ class CreatePopularityFunction < ActiveRecord::Migration
 CREATE OR REPLACE FUNCTION ranking(created_at timestamp, vote_count integer) RETURNS NUMERIC AS $$
   SELECT ROUND(LOG(2, greatest(vote_count, 1)) + ((EXTRACT(EPOCH FROM created_at) - EXTRACT(EPOCH from timestamp '2014-1-1 0:00')) / 450000)::numeric, 7);
 $$ LANGUAGE SQL IMMUTABLE;
+SPROC
 
+    execute <<-SPROC
 CREATE INDEX index_questions_on_ranking
   ON questions (ranking(created_at::timestamp, vote_count) DESC);
 SPROC
