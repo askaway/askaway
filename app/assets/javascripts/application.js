6e46964cfb3253a98e76597fdddfe1fc929e36dc
Rowan Crawford <rowan.crawford@joukou.co.nz>
2014-06-18 20:50:47 +1200

    Add angular to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+// This is a manifest file that'll be compiled into application.js, which will include all the files // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
@@ -12,6 +11,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require angular
 //= require bootstrap
 //= require_tree .
 
