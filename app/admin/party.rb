516ddc143a90d6bf52f70c3c0c350ca8262c3a27
Jon Lemmon <jon@loomio.org>
2014-07-02 19:17:28 +1200

    Fix party admin dashboard stuff

--- app/admin/party.rb
+++ app/admin/party.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Party do
   permit_params :name, :auth_statement, :description
+
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+  end
 end
