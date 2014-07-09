db3d7cb4ce8de435d1734ff58ef9d680ead1603d
Jon Lemmon <jon@loomio.org>
2014-07-08 21:36:09 -0700

    Fix twitter login

--- app/services/o_auth_service.rb
+++ app/services/o_auth_service.rb
@@ -26,7 +26,7 @@ class OAuthService
         user = User.new(
           name: auth.extra.raw_info.name,
           #username: auth.info.nickname || auth.uid,
-          email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+          email: email ? email : "#{User::TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0,20]
         )
         user.save!
