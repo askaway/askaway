67b28714f280b0ace7d6620e9b0a6b8cb1bb506f
Cameron Prebble <cameron.prebble@abletech.co.nz>
2014-08-05 21:38:22 +1200

    prevent getSelection from breaking IE8

--- app/assets/javascripts/controllers/questions.js
+++ app/assets/javascripts/controllers/questions.js
@@ -9,7 +9,7 @@ askaway.controller('QuestionsCtrl', ['$scope', '$http', function( $scope, $http 
     var $target = $(e.target),
       $veto = $target.closest('a[href], form');
 
-    if (window.getSelection().toString() != '') return;
+    if (window.getSelection && window.getSelection().toString() != '') return;
 
     if ($veto.length === 0) {
       this.question.expanded = !this.question.expanded;
