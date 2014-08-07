fc9a4f1d11075e760876438e74b541b601fa0569
Jon Lemmon <jon@loomio.org>
2014-08-07 14:53:15 +1200

    Improve rep page

--- app/presenters/profile_presenter.rb
+++ app/presenters/profile_presenter.rb
@@ -1,4 +1,6 @@
 class ProfilePresenter
+  attr_reader :user, :record
+
   def initialize(user, record)
     @user = user
     @record = record
@@ -18,6 +20,18 @@ class ProfilePresenter
     @record.name
   end
 
+  def link_url
+    if record_is_rep?
+      Rails.application.routes.url_helpers.party_path(@record.party)
+    end
+  end
+
+  def link_text
+    if record_is_rep?
+      @record.party.name
+    end
+  end
+
   def description
     if @record.class == Party
       @record.description
@@ -26,12 +40,17 @@ class ProfilePresenter
 
   def sections
     if @record.class == Party
-      section = ProfileSectionPresenter.new(name: 'spokespeople',
-                                            record: @record)
+      section = ProfileSectionPresenter.new(name: 'spokespeople', record: @record)
+    elsif record_is_rep?
+      section = ProfileSectionPresenter.new(name: 'answers', record: @record)
     elsif @record.class == User
-      section = ProfileSectionPresenter.new(name: 'questions',
-                                            record: @record)
+      section = ProfileSectionPresenter.new(name: 'questions', record: @record)
     end
     [section]
   end
+
+  private
+    def record_is_rep?
+      @record.class == User && @record.is_rep?
+    end
 end
