ee6cbd187888585953b4c2972c5803781a236c75
Rowan Crawford <wombleton@gmail.com>
2014-08-11 22:08:52 +1200

    After first time hitting the end of questions, don't try and load any more.

--- app/assets/javascripts/controllers/questions.js
+++ app/assets/javascripts/controllers/questions.js
@@ -33,6 +33,10 @@ askaway.controller('QuestionsCtrl', ['$scope', '$http', function( $scope, $http 
   };
 
   $scope.loadQuestions = function() {
+    if ($scope.noMoreQuestions) {
+      return;
+    }
+
     var url = getUrl();
 
     $scope.loadingQuestions = true;
@@ -43,6 +47,10 @@ askaway.controller('QuestionsCtrl', ['$scope', '$http', function( $scope, $http 
       $scope.loadingQuestions = false;
       for (; i < data.length; i++) {
         $scope.questionList.push(data[i]);
+      }
+
+      if (data.length === 0) {
+        $scope.noMoreQuestions = true;
       }
 
       $scope.questionsInitialized = true;
