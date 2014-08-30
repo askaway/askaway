4b1ecb03d6ebf3cba4ed0dec94a4eebf6d447698
Rowan Crawford <wombleton@gmail.com>
2014-08-30 21:55:47 +1200

    Use initCalled flag.

--- app/assets/javascripts/directives/fb_share.js
+++ app/assets/javascripts/directives/fb_share.js
@@ -6,17 +6,17 @@ askaway.directive('fbshare', [ '$window', '$timeout', function($window, $timeout
         key;
 
       function loadShare() {
-        if (!$window.fbAsyncInit || !$window.FB) {
+        if (!$window.FB || !$window.FB.initCalled) {
           delay *= 2;
           return $timeout(loadShare, delay);
         }
 
-        FB.XFBML.parse($el.parent()[0]);
+        FB.XFBML.parse($el[0]);
       }
 
       angular.element($el).attr({
         'data-layout': 'button_count',
-        'data-href': loc.origin + $attrs.path,
+        'data-href': 'http://askaway.org.nz' + $attrs.path,
         'data-action': 'like',
         'data-show-faces': 'false',
         'data-share': 'false'
